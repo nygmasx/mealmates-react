d46e285d73e49787565e1ddb52e167dbbddeed1d
lakpoo <kiliandominik93@gmail.com>
2025-03-13 19:56:14 +0100

    page inscription

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { GalleryVerticalEnd } from "lucide-react";
 import { LoginForm } from "@/components/login-form.jsx";
 import { ConnexionForm } from "@/components/connexion-form.jsx";
+import { InscriptionForm } from "@/components/inscription-form.jsx";
 
 function App() {
   return (
@@ -9,9 +10,16 @@ function App() {
     //     <LoginForm />
     //   </div>
     // </div>
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-right-top bg-no-repeat p-6">
+
+    // <div className="flex min-h-svh flex-col items-center gap-6 bg-right-top bg-no-repeat p-6">
+    //   <div className="flex w-full max-w-2xl flex-col gap-6">
+    //     <ConnexionForm />
+    //   </div>
+    // </div>
+
+    <div className="flex min-h-svh flex-col items-center gap-6 bg-right-top bg-no-repeat p-6">
       <div className="flex w-full max-w-2xl flex-col gap-6">
-        <ConnexionForm />
+        <InscriptionForm />
       </div>
     </div>
   );
--- src/components/inscription-form.jsx
+++ src/components/inscription-form.jsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-export function ConnexionForm({ className, ...props }) {
+export function InscriptionForm({ className, ...props }) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <form>
@@ -15,19 +15,23 @@ export function ConnexionForm({ className, ...props }) {
           </div>
           <div className="flex flex-col gap-10">
             <div className="flex flex-col left-center gap-2">
-              <h1 className="text-3xl font-medium">Connexion</h1>
-              <p className="text-m text-gray-500 font-medium">Entrez votre email et mot de passe</p>
+              <h1 className="text-3xl font-medium">Inscription</h1>
+              <p className="text-m text-gray-500 font-medium">Entrez vos informations d'identification</p>
             </div>
             <div className="flex flex-col gap-6 max-w-m">
               <div className="grid gap-3">
-                <Label className="text-gray-500">Email</Label>
-                <Input id="email" type="email" placeholder="fethiammar@mealmate.fr" required className="p-0" />
+                <Label className="text-gray-500">Nom d'utilisateur</Label>
+                <Input id="username" type="username" placeholder="Fethi Ammar" required className="p-0" />
               </div>
               <div className="grid gap-3">
-                <Label className="text-gray-500">Mot de passe </Label>
+                <Label className="text-gray-500">Email</Label>
+                <Input id="email" type="email" placeholder="fethiammar@mealmates.fr" required className="p-0" />
+              </div>
+              <div className="grid gap-3">
+                <Label className="text-gray-500">Mot de passe</Label>
                 <Input id="password" type="password" placeholder="● ● ● ● ● ●" required className="p-0" />
               </div>
-              <p className="text-m text-right">Mot de passe oublier ?</p>
+              <p className="text-gray-500">En continuant, vous acceptez nos conditions de service et notre politique de confidentialité.</p>
             </div>
           </div>
           <div className="grid gap-4 flex-col">
@@ -35,9 +39,9 @@ export function ConnexionForm({ className, ...props }) {
               Se connecter
             </Button>
             <p className="text-center">
-              Vous n'avez pas de compte ?{" "}
+              Vous avez déjà un compte ?{" "}
               <a href="" className="text-button-green">
-                S'inscire
+                S'identifier
               </a>
             </p>
           </div>
