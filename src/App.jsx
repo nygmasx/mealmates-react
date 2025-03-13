f4687d23a46d320b7efe672050efc651a64dc91e
lakpoo <kiliandominik93@gmail.com>
2025-03-13 20:05:39 +0100

    fix logo premiere page et ajustemement

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,29 @@ import { InscriptionForm } from "@/components/inscription-form.jsx";
 
 function App() {
   return (
-    // <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-[url(../assets/sac-course.png)] bg-right-top bg-no-repeat p-6">
-    //   <div className="flex w-full max-w-2xl flex-col gap-6">
-    //     <LoginForm />
-    //   </div>
-    // </div>
+    // ACCUEIL
+    <div className="flex min-h-svh flex-col items-center pt-40 gap-6 bg-[url(../assets/sac-course.png)] bg-right-top bg-no-repeat p-6">
+      <div>
+        <img src="/assets/logo-mealmate.png" alt="Logo MealMate" className="max-w-2/5 mx-auto" />
+      </div>
+      <div className="flex w-full max-w-2xl flex-col gap-6">
+        <LoginForm />
+      </div>
+    </div>
 
+    //CONNEXION
     // <div className="flex min-h-svh flex-col items-center gap-6 bg-right-top bg-no-repeat p-6">
     //   <div className="flex w-full max-w-2xl flex-col gap-6">
     //     <ConnexionForm />
     //   </div>
     // </div>
 
-    <div className="flex min-h-svh flex-col items-center gap-6 bg-right-top bg-no-repeat p-6">
-      <div className="flex w-full max-w-2xl flex-col gap-6">
-        <InscriptionForm />
-      </div>
-    </div>
+    //INSCRIPTION
+    // <div className="flex min-h-svh flex-col items-center gap-6 bg-right-top bg-no-repeat p-6">
+    //   <div className="flex w-full max-w-2xl flex-col gap-6">
+    //     <InscriptionForm />
+    //   </div>
+    // </div>
   );
 }
 
